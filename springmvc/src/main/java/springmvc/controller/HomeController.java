cfd85bdabb62a2b98f5d5ed9f8221a0f03ffd7ca
Sahillokhande333 <lokhandesahil2019@gmail.com>
2024-03-03 02:27:58 +0530

    modify with model

--- springmvc/src/main/java/springmvc/controller/HomeController.java
+++ springmvc/src/main/java/springmvc/controller/HomeController.java
@@ -1,16 +1,28 @@
 package springmvc.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
 	@RequestMapping("/home")
-	public String home()
+	public String home(Model model)
 	{
 		System.out.println("This is home url");
+		model.addAttribute("name" ,"Sahil Lokhande here");
+		model.addAttribute("age", 22);
 		
+		List<String> friends=new ArrayList<String>();
+		friends.add("Gayatri");
+		friends.add("Bhushan");
+		friends.add("Babu");
+		
+		model.addAttribute("f",friends);
 		return "index";
 		
 	}
--- springmvc/src/main/webapp/WEB-INF/views/index.jsp
+++ springmvc/src/main/webapp/WEB-INF/views/index.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+    <%@ page import=java.util.List %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,5 +11,24 @@
   <h1>This is home page</h1>
   <h1>Called by home controller</h1>
   <h1>url /home</h1>
+  
+  <%
+  String name=(String) request.getAttribute("name");
+  Integer age=(Integer) request.getAttribute("age");
+  
+  List<String> friends= (List<String>) request.getAttribute("f");
+  %>
+  
+  
+  <h1>Name is: <%=name %></h1>
+  
+  <h1>Age is: <%=age %></h1>
+  
+  <%
+  	for (String s: friends){
+  %>
+  <h1><%=s %></h1>
+  <%} %>
+  
 </body>
 </html>
