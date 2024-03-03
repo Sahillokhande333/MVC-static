ac0d481730d6f8b4b41dfc1117e0c310c79d5bda
Sahillokhande333 <lokhandesahil2019@gmail.com>
2024-03-03 22:56:36 +0530

    here i used model and ModelAndView methods

--- springmvc/src/main/java/springmvc/controller/HomeController.java
+++ springmvc/src/main/java/springmvc/controller/HomeController.java
@@ -1,11 +1,13 @@
 package springmvc.controller;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -35,5 +37,20 @@ public class HomeController {
 		return "about";
 		
 	}
+	//ModelAndView 
+	
+	@RequestMapping("/help")
+	public ModelAndView help(){
+		System.out.println("This is help controller");
+		ModelAndView modelAndView= new ModelAndView();
+		modelAndView.addObject("name", "Sakshi");
+		modelAndView.addObject("num",20);
+		modelAndView.setViewName("help");
+		
+		LocalDateTime now= LocalDateTime.now();
+		modelAndView.addObject("time",now);
+		return modelAndView;
+		
+		}
 
 }
---
+++ springmvc/src/main/webapp/WEB-INF/views/help.jsp
@@ -0,0 +1,23 @@
+<%@page import="java.time.LocalDateTime"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Help page</title>
+</head>
+<body>
+
+<%
+	String name=(String)request.getAttribute("name");
+    Integer num=(Integer)request.getAttribute("num");
+    LocalDateTime time=(LocalDateTime)request.getAttribute("time");
+
+%>
+	<h1>This is help page</h1>
+	<h1>My name is: <%=name %></h1>
+	<h1>for contact please call us on:<%=num %></h1>
+	<h1>Fate and Time is:<%=time %></h1>
+</body>
+</html>
